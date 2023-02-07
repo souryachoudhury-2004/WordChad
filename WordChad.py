69688f17ba19e657366d96cd1f696f713f55ae9d
Sourya Choudhury <souryachoudhury24@gmail.com>
2023-02-07 15:54:27 +0530

    Added more functionality.

--- WordChad.py
+++ WordChad.py
@@ -5,8 +5,9 @@ sg.change_look_and_feel("DarkAmber")
 text_entry = sg.Multiline(key="text_written", size= (68, 21), font=["Times New Roman", 20])
 save_button = sg.Button("Save", key="save")
 selector_button = sg.FilesBrowse("Choose File Name", key="select")
+open_button = sg.Button("Open", key="open")
 
-layout = [[text_entry], [save_button, selector_button]]
+layout = [[text_entry], [save_button, selector_button, open_button]]
 
 window = sg.Window("WordChad", layout=layout, size=(1000, 700))
 
@@ -14,6 +15,19 @@ while True:
     event, values = window.read()
     print(event)
     print(values)
+
+    try:
+        if event == "open":
+            filepath = values["select"]
+            if filepath == "":
+                sg.popup("Please pick a file to open!")
+                continue
+            with open(filepath, "r") as file:
+                text_to_update = file.read()
+                window["text_written"].update(text_to_update)
+    except:
+        sg.popup("WordChad has run into an issue. Please check if the file is of .txt type.")
+
     if event == "save":
         fileWrite = values["text_written"]
         filepath = values["select"]
